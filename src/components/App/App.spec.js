4af00c1d62f8835da88c1bca60d9af26cd527769
Liz Daly <liz7daly@gmail.com>
2020-04-22 17:44:18 +0100

    Test for calculator element

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import Calculator from '../Calculator/Calculator'
 
 describe('App', () => {
   let wrapper;
@@ -9,5 +10,9 @@ describe('App', () => {
 
   it('should render a <div />', () => {
     expect(wrapper.find('div').length).toEqual(1);
+  });
+
+  it('should render the Calculator Component', () => {
+    expect(wrapper.containsMatchingElement(<Calculator />)).toEqual(true);
   });
 });
