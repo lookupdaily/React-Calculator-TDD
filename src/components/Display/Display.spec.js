6c643db846324e7a25d74b7860dc4bc0c54000fe
Liz Daly <liz7daly@gmail.com>
2020-04-22 19:13:47 +0100

    Style display

--- src/components/Display/Display.spec.js
+++ src/components/Display/Display.spec.js
@@ -7,6 +7,8 @@ describe('Display', () => {
 
   beforeEach(() => wrapper = shallow(<Display displayValue={''}/>));
 
+  it('should render correctly', () => expect(wrapper).toMatchSnapshot());
+
   it('should render a <div />', () => {
     expect(wrapper.find('div').length).toEqual(1);
   });
