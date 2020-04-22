d767c8f1f66eb743476519f4062d50bb5ac62a7a
Liz Daly <liz7daly@gmail.com>
2020-04-22 18:00:56 +0100

    Render Calculator as stateful component

---
+++ src/components/Calculator/calculator.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  state = {
+    displayValue: 0,
+    numbers: [],
+    operators: [],
+    selectedOperator: '',
+    storedValue: '',
+  }
+
+  callOperator = () => {
+    console.log('call operation');
+  }
+
+  setOperator = () => {
+    console.log('set operation');
+  }
+
+  updateDisplay = () => {
+    console.log('update display');
+  }
+
+  render = () => {
+    return (
+      <div className="calculator-container" />
+    );
+  }
+}
+
+export default Calculator;
