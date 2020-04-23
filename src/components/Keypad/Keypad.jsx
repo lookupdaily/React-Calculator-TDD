fde07e4806e97a9647a5da5750c6738def31d8ba
Liz Daly <liz7daly@gmail.com>
2020-04-23 11:59:08 +0100

    Test for rendering operators

--- src/components/Keypad/Keypad.jsx
+++ src/components/Keypad/Keypad.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Keypad = ({ callOperator, numbers, operators, setOperator, updateDisplay }) => 
-  <div className="keypad-container" />;
+const Keypad = ({ 
+  callOperator, 
+  numbers, 
+  operators, 
+  setOperator, 
+  updateDisplay 
+}) => {
+  const numberKeys = numbers.map(number => <p key={number}>{number}</p>);
+
+  const operatorKeys = operators.map(operator => <p key={operator}>{operator}</p>);
+
+  return (
+    <div className="keypad-container">
+      <div className="numbers-container">
+        {numberKeys}
+      </div>
+      <div className="operators-container">
+        {operatorKeys}
+      </div>
+    </div>
+  );
+}
 
 Keypad.propTypes = {
   callOperator: PropTypes.func.isRequired,
@@ -10,6 +30,6 @@ Keypad.propTypes = {
   operators: PropTypes.array.isRequired,
   setOperator: PropTypes.func.isRequired, 
   updateDisplay: PropTypes.func.isRequired,
-}
+};
 
 export default Keypad;
--- src/components/Keypad/Keypad.spec.js
+++ src/components/Keypad/Keypad.spec.js
@@ -17,12 +17,17 @@ describe('Keypad', () => {
     );
   });
 
-  it('should render a <div />', () => {
-    expect(wrapper.find('div').length).toEqual(1);
+  it('should render 3 <div />s', () => {
+    expect(wrapper.find('div').length).toEqual(3);
   });
 
   it('renders the values of numbers', () => {
     wrapper.setProps({numbers: ['0', '1', '2']});
     expect(wrapper.find('.numbers-container').text()).toEqual('012');
+  })
+
+  it('renders the value of operators', () => {
+    wrapper.setProps({operators: ['+','-','*','/']});
+    expect(wrapper.find('.operators-container').text()).toEqual('+-*/');
   })
 });
