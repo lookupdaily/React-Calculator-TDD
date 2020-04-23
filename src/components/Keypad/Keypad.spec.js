21e0367f783bdb160452973b5893d95e8908e5d2
Liz Daly <liz7daly@gmail.com>
2020-04-23 14:33:20 +0100

    Test for instances of keys of all types

--- src/components/Keypad/Keypad.spec.js
+++ src/components/Keypad/Keypad.spec.js
@@ -32,7 +32,12 @@ describe('Keypad', () => {
     expect(wrapper.find('.operators-container').text()).toEqual('+-*/');
   })
 
-  it('should render an instance of the Key component', () => {
-    expect(wrapper.find('Key').length).toEqual(1);
+  it('should render an instance of the Key component for each index of numbers, operators and the submit key', () => {
+    const numbers = ['0', '1'];
+    const operators = ['+', '-'];
+    const submit= 1;
+    const keyTotal = numbers.length + operators.length + submit;
+    wrapper.setProps({numbers, operators});
+    expect(wrapper.find('Key').length).toEqual(keyTotal);
   });
 });
