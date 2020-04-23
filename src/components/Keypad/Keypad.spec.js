809aca6bff09e27d9dab1775f4e4961bdf2d8ad2
Liz Daly <liz7daly@gmail.com>
2020-04-23 11:34:11 +0100

    Test for rendering numbers

--- src/components/Keypad/Keypad.spec.js
+++ src/components/Keypad/Keypad.spec.js
@@ -20,4 +20,9 @@ describe('Keypad', () => {
   it('should render a <div />', () => {
     expect(wrapper.find('div').length).toEqual(1);
   });
+
+  it('renders the values of numbers', () => {
+    wrapper.setProps({numbers: ['0', '1', '2']});
+    expect(wrapper.find('.numbers-container').text()).toEqual('012');
+  })
 });
